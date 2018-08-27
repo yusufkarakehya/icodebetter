a7af5ed2db29239ddb4358cc85708b40775061e0
Azat Temirbek uulu <e178551@gmail.com>
2018-08-27 11:50:09 +0300

    masonry Component added but not tested

--- src/main/resources/static/react/js/iwb-nifty.js
+++ src/main/resources/static/react/js/iwb-nifty.js
@@ -816,6 +816,115 @@ class GridCommon extends React.PureComponent {
     };
   }
 }
+/**
+ * A component to render Masonry layout
+ * @example
+ * <XMasonry>{!!this.state.photos && this.state.photos.map(photo =>( <img key={photo.id}  src={photo.url}/> ) )} </XMasonry>
+ */
+class XMasonry extends React.PureComponent {
+	
+	gridRef = null;
+	gridFilledBoundary = [];
+	constructor() {
+		super();
+		this.initArray = (size) => {
+			return Array(size).fill(0)
+		}
+		/**
+		 *  Loop through the gridFilledBoundary
+		 *  Find optimal position (Magic?)
+		 *  Min(y) that satisfies for x to x + element.offsetWidth
+		 *  If y found that satisfies, skip till next y < currentMinY
+		 *  Update gridFilledBoundary
+		 *
+		 * @param {HTMLElement} element
+		 */
+		this.addChild = (element) => {
+			let optimalPosition = {
+				x: 0,
+				y: Math.max(...this.gridFilledBoundary)
+			};
+			const width = element.offsetWidth;
+			for (let x = 0; x < this.gridFilledBoundary.length - width; x++) {
+				const columnHeight = this.gridFilledBoundary[x];
+				// For compact stacking, minimize Y, Early return if columnHeight > foundMinY
+				if (columnHeight >= optimalPosition.y) continue;
+				// Element can fit at given Y co-ordianate
+				if (this.canFit(x, element)) {
+					optimalPosition = {
+						x,
+						y: columnHeight,
+					};
+				}
+			}
+			this.updateGridBoundary(element, optimalPosition);
+			requestAnimationFrame(() => {
+				this.placeElement(element, optimalPosition);
+			})
+		}
+		/**
+		 * Checks if an element can fit at Position(x)
+		 *
+		 * @param {Number} x
+		 * @param {HTMLElement} element
+		 */
+		this.canFit = (x, element) => {
+			const gridFilledBoundary = this.gridFilledBoundary;
+			const width = element.offsetWidth;
+			const y = gridFilledBoundary[x];
+			const portion = gridFilledBoundary.slice(x, x + width);
+			return portion.every(value => value <= y);
+		}
+		this.placeElement = (element, position) => {
+			element.style.top = `${position.y}px`;
+			element.style.left = `${position.x}px`;
+		}
+		this.updateGridBoundary = (element, position) => {
+			const width = element.offsetWidth;
+			const boundaryY = position.y + element.offsetHeight;
+			for (let x = position.x; x < position.x + width; x++) {
+				this.gridFilledBoundary[x] = boundaryY;
+			}
+		}
+		this.layout = () => {
+			if (!this.gridRef) return;
+			this.gridFilledBoundary = this.initArray(this.gridRef.offsetWidth);
+			const children = this.gridRef.children;
+			for (let child of children) { this.addChild(child) }
+		}
+	}
+	componentDidUpdate() { this.layout() }
+	componentDidMount() { 
+		window.addEventListener('resize', this.layout);
+		setTimeout(()=> this.layout(), 1000);
+	}
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.layout);
+	}
+	render() {
+		return (React.createElement("div", {
+					className: "masonry-grid",
+					style: {
+						position: "relative"
+					},
+					ref: node => (this.gridRef = node)
+				},
+				React.Children.map(this.props.children, child =>
+					React.createElement(
+						"div", {
+							className: "grid-item",
+							style: {
+								position: "absolute",
+								transition: "top 400ms cubic-bezier(0.455, 0.03, 0.515, 0.955), left 400ms cubic-bezier(0.455, 0.03, 0.515, 0.955)",
+							}
+						},
+						child
+					)
+				)
+			)
+		);
+	}
+}
 
 /**
  * XAjaxQueryData - function is used to get data by giving guery id
