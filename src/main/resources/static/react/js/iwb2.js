8cc55b334a626697edb92548c0f09be45137252b
Azat Temirbek uulu <e178551@gmail.com>
2018-08-31 14:56:55 +0300

    XLazyScriptLoader version 2 rewrite

--- src/main/resources/static/react/js/iwb2.js
+++ src/main/resources/static/react/js/iwb2.js
@@ -1435,108 +1435,102 @@ class XAjaxQueryData extends React.PureComponent {
 /**
  * A function to load script from the CDN or filesystem and apply css
  * @param {String} props.css - query id that you want to get data from
- * @param {Array/Object} props.load - used to define wich script to download see exapmle below
+ * @param {Array/String} props.loadjs - used to define which script to download see exapmle below
+ * @param {Array/String} props.loadcss - used to define which css script to download see exapmle below
  * @param {Symbol} props.loading - conponent to show loading indicator while feching scripts from CDN or static file
  * @param {Symbol} props.children
  * @example
- *  _(XLazyScriptLoader,{loading:React.createElement(CustomLoadingComponent,{options}),css:`.customClassName{color:red}`, load:[{name:'libName',src:'CDN'},{name:'libName',src:'CDN'}]||{name:'libName',src:'CDN'} }, childNode )
+ *  _(XLazyScriptLoader,{loading:React.createElement(CustomLoadingComponent,{options}),css:`.customClassName{color:red}`, loadjs:['CDN','CDN2']||'CDN' }, childNode )
  */
-class XLazyScriptLoader extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true
-    };
-    props.load === null &&
-      console.warn(`provide load props to LazyScriptLoader Component `);
-    this.ListOfScripts = [];
-    this.head = document.head || document.getElementsByTagName("head")[0];
-    this.xscript = document.getElementsByTagName("script")[0];
-    /** METHODS */
-    this.onScriptLoad = () => {
-      var problem = false;
-      this.ListOfScripts.map(nameAndSrc => {
-        if (!window[nameAndSrc.name]) {
-          problem = true;
-          console.warn(
-            "problem with ${" +
-              nameAndSrc.name +
-              "} script there is no such script on window object"
-          );
-          return null;
+class XLazyScriptLoader extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true
         }
-        return nameAndSrc;
-      });
-      if (
-        !!this.props.load &&
-        this.props.load.constructor === Array &&
-        this.ListOfScripts.length === this.props.load.length &&
-        !problem
-      ) {
-        this.setState({ loading: false });
-      } else if (
-        !!this.props.load &&
-        this.props.load.constructor === Object &&
-        this.ListOfScripts.length === 1 &&
-        !problem
-      ) {
-        this.setState({ loading: false });
-      }
-    };
-    this.loadScript = ({ name, src }) => {
-      let script = document.createElement("script");
-      script.id = "script-" + name;
-      script.type = "text/javascript";
-      script.async = false;
-      script.src = src;
-      this.xscript.parentNode.insertBefore(script, this.xscript);
-      return script;
-    };
-    this.CSSIntoDom = (css = "") => {
-      let style = document.createElement("style");
-      style.type = "text/css";
-      if (style.styleSheet) {
-        style.styleSheet.cssText = css;
-      } else {
-        style.appendChild(document.createTextNode(css));
-      }
-      this.head.appendChild(style);
-    };
-  }
-  componentDidMount() {
-    if (!!this.props.load && this.props.load.constructor === Array) {
-      // [{name:'Azat',src:'source code'},{name:'Azat',src:'source code'}]
-      this.props.load.map(nameAndSrc => {
-        this.ListOfScripts = [...this.ListOfScripts, nameAndSrc];
-        if (!window[nameAndSrc.name]) {
-          let script = this.loadScript(nameAndSrc);
-          script.addEventListener("load", e => this.onScriptLoad());
-        } else this.onScriptLoad();
-        return nameAndSrc;
-      });
-    } else if (!!this.props.load && this.props.load.constructor === Object) {
-      // {name:'Azat',src:'source code'}
-      this.ListOfScripts = [...this.ListOfScripts, this.props.load];
-      if (!window[this.props.load.name]) {
-        let script = this.loadScript(this.props.load);
-        script.addEventListener("load", e => this.onScriptLoad());
-      } else this.onScriptLoad();
-    } else {
-      console.warn(
-        `provide load={name:'Azat',src:'source code'} to LazyScriptLoader`
-      );
+        /**
+         * A function to insert css classes into Dom
+         * @param {string} css - example '.aclass{display:none}'
+         */
+        this.CSSIntoDom = (css = '') => {
+            let style = document.createElement('style');
+            style.type = 'text/css';
+            (style.styleSheet)? style.styleSheet.cssText = css : style.appendChild(document.createTextNode(css))
+            document['head'].appendChild(style);
+        }
+        /**
+         * a self invoking function to load js and css into Dom from source {cdn,server,local.....}
+         */
+        this.load = (() => {
+            // Function which returns a function: https://davidwalsh.name/javascript-functions
+            var _load = (tag) => {
+                return (src) => {
+                    // This promise will be used by Promise.all to determine success or failure
+                    return new Promise( (resolve, reject) => {
+                        let element = document.createElement(tag);
+                        let parent = 'body';
+                        let attr = 'src';
+                        // Important success and error for the promise
+                        element.onload = e => resolve(src);
+                        element.onerror = e => reject(src);
+                        // Need to set different attributes depending on tag type
+                        switch (tag) {
+                            case 'script':
+                                element.async = true;
+                                break;
+                            case 'link':
+                                element.type = 'text/css';
+                                element.rel = 'stylesheet';
+                                attr = 'href';
+                                parent = 'head';
+                                break;
+                            default:
+                        }
+                        // Inject into document to kick off loading
+                        element[attr] = src;
+                        window.document[parent].appendChild(element);
+                    });
+                };
+            }
+            return {
+                css: _load('link'),
+                js:  _load('script'),
+                img: _load('img')
+            }
+        })();
     }
-    this.CSSIntoDom(this.props.css);
-  }
-  render() {
-    return React.createElement(
-      React.Fragment,
-      {},
-      this.state.loading && this.props.loading,
-      !this.state.loading && this.props && this.props.children
-    );
-  }
+    componentDidMount() {
+        let arrayProm = []
+        let {loadcss,loadjs, css} = this.props;
+        loadcss && arrayProm.push(...(loadcss.constructor === Array)?loadcss.map(item=>this.load.css(item)):[this.load.css(loadcss)]);
+        loadjs && arrayProm.push(...(loadjs.constructor === Array)?loadjs.map(item=>this.load.js(item)):[this.load.js(loadjs)]);
+        Promise.all(arrayProm).then(() => {
+            this.setState({ loading: false})
+        }).catch(() => {
+            console.error('Oh no, epic failure!');
+            alert('Oh no, epic failure!');
+        });
+        this.CSSIntoDom(css)
+    }
+    render() {
+        return React.createElement(React.Fragment, {},
+            this.state.loading && this.props.loading,
+            !this.state.loading && this.props && this.props.children,
+        )
+    }
 }
+XLazyScriptLoader.propTypes = {
+    css: PropTypes.string,
+    loadjs: PropTypes.oneOfType([
+        PropTypes.array,
+        PropTypes.string,
+    ]),
+    loadcss: PropTypes.oneOfType([
+        PropTypes.array,
+        PropTypes.string,
+    ]),
+    children: PropTypes.node.isRequired
+};
 
 class XFormSMSEmailTemplateList extends React.PureComponent {
   constructor(props) {
