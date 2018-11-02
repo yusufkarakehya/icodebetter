a93575c259e8564729ca7ff5f997ec8927c9e132
Azat Temirbek uulu <e178551@gmail.com>
2018-11-02 09:38:21 +0300

    monaco width and height

--- src/main/resources/static/monaco/icb-monaco.js
+++ src/main/resources/static/monaco/icb-monaco.js
@@ -25,13 +25,18 @@ Ext.ux.form.Monaco = Ext.extend(Ext.BoxComponent, {
           console.log("self.editor: ", self.editor);
           */
             var containerHeight = self.ownerCt.container.dom.clientHeight;
+            var containerWidth = self.ownerCt.container.dom.clientWidth;
 
-            if (containerHeight < 500) {
+            if (containerHeight < 500 || containerWidth < 500 ) {
               containerHeight = 500;
+              containerWidth = 500;
               self.ownerCt.setHeight(500);
+              self.ownerCt.setWidth(500);
               self.ownerCt.container.setHeight(500);
+              self.ownerCt.container.setWidth(500);
             }
             self.setHeight(containerHeight);
+            self.setWidth(containerWidth);
             self.editor.layout();
 
             var itemLength = self.ownerCt.items.items.length;
@@ -73,7 +78,9 @@ Ext.ux.form.Monaco = Ext.extend(Ext.BoxComponent, {
             window.onresize = function() {
               self.editor.layout();
               containerHeight = self.ownerCt.container.dom.clientHeight;
+              containerWidth = self.ownerCt.container.dom.clientWidth;
               self.setHeight(containerHeight);
+              self.setWidth(containerWidth);
               self.editor.layout();
             };
           });
