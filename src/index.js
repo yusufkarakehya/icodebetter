12c39336ecc9e4be86897bf99a8c7569f44aadd9
e178551 <e178551@metu.edu.tr>
2019-09-18 16:58:01 +0300

    Demo components for treedropdown

--- src/index.js
+++ src/index.js
@@ -1,9 +1,73 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import XHTMLEditor from "./react/lib/XHTMLEditor";
-// import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { XDropdownTreeSelect, XDropdownMultiTreeSelect } from './react/lib/XDropdownTreeSelect';
+var a = {
+    label: 'searchme1',
+    value: 'searchme1',
+    children: [
+        {
+            label: 'searchme2',
+            value: 'searchme2',
+            children: [
+                {
+                    label: 'searchme3',
+                    value: 'searchme3',
+                },
+                {
+                    label: 'searchme3.2',
+                    value: 'searchme3.2',
+                },
+            ],
+        },
+        {
+            label: 'searchme2.2',
+            value: 'searchme2.2',
+            children: [
+                {
+                    label: 'searchme5',
+                    value: 'searchme5',
+                },
+                {
+                    label: 'searchme5.2',
+                    value: 'searchme5.2',
+                },
+            ],
+        },
+    ],
+}
 
-ReactDOM.render(<XHTMLEditor />, document.getElementById("root"));
+class PropsChanger extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value:
+            {
+                checked: true,
+                hide: false,
+                label: "searchme2",
+                value: "searchme2",
+            }
+
+        }
+    }
+    componentDidMount() {
+        // setTimeout(() =>{
+        //     alert('hello')
+        //     this.setState({value: a})
+        // },8000)
+    }
+    render() {
+        return (
+            <div>
+                <XDropdownMultiTreeSelect defaultList={a} value={this.state.value} onChange={(a) => { console.log(a); this.setState({ value: a }) }} />
+                <XDropdownTreeSelect defaultList={a} value={this.state.value} onChange={(a) => { console.log(a); this.setState({ value: a }) }} />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <PropsChanger />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
