6ca6cb68ae0657fa3f903f7087c0ea68a6b1834d
e178551 <e178551@metu.edu.tr>
2019-09-19 11:35:58 +0300

    Demo page

--- src/index.js
+++ src/index.js
@@ -1,73 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { XDropdownTreeSelect, XDropdownMultiTreeSelect } from './react/lib/XDropdownTreeSelect';
-var a = {
-    label: 'searchme1',
-    value: 'searchme1',
-    children: [
-        {
-            label: 'searchme2',
-            value: 'searchme2',
-            children: [
-                {
-                    label: 'searchme3',
-                    value: 'searchme3',
-                },
-                {
-                    label: 'searchme3.2',
-                    value: 'searchme3.2',
-                },
-            ],
-        },
-        {
-            label: 'searchme2.2',
-            value: 'searchme2.2',
-            children: [
-                {
-                    label: 'searchme5',
-                    value: 'searchme5',
-                },
-                {
-                    label: 'searchme5.2',
-                    value: 'searchme5.2',
-                },
-            ],
-        },
-    ],
-}
-
-class PropsChanger extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            value:
-            {
-                checked: true,
-                hide: false,
-                label: "searchme2",
-                value: "searchme2",
-            }
-
-        }
-    }
-    componentDidMount() {
-        // setTimeout(() =>{
-        //     alert('hello')
-        //     this.setState({value: a})
-        // },8000)
-    }
-    render() {
-        return (
-            <div>
-                <XDropdownMultiTreeSelect defaultList={a} value={this.state.value} onChange={(a) => { console.log(a); this.setState({ value: a }) }} />
-                <XDropdownTreeSelect defaultList={a} value={this.state.value} onChange={(a) => { console.log(a); this.setState({ value: a }) }} />
-            </div>
-        )
-    }
-}
+import "@fortawesome/fontawesome-free/css/all.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import { DropdownTreeSelectDemo } from './react/lib/XDropdownTreeSelect/DropdownTreeSelectDemo'
 
 ReactDOM.render(
-    <PropsChanger />, document.getElementById("root"));
+    <DropdownTreeSelectDemo />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/react/lib/XDropdownTreeSelect/DropdownTreeSelectDemo.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import { XDropdownTreeSelect, XDropdownMultiTreeSelect, XDropdownTreeSelectResult } from './';
+import { Col, Form, FormGroup, Label, Input } from 'reactstrap';
+import "../css/styles.alba.css";
+import "../css/iwb-nifty.css";
+import "../css/style.css";
+var a = {
+    label: 'searchme1',
+    value: 'searchme1',
+    children: [
+        {
+            label: 'searchme2',
+            value: 'searchme2',
+            children: [
+                {
+                    label: 'searchme3',
+                    value: 'searchme3',
+                },
+                {
+                    label: 'searchme3.2',
+                    value: 'searchme3.2',
+                },
+            ],
+        },
+        {
+            label: 'searchme2.2',
+            value: 'searchme2.2',
+            children: [
+                {
+                    label: 'searchme5',
+                    value: 'searchme5',
+                },
+                {
+                    label: 'searchme5.2',
+                    value: 'searchme5.2',
+                },
+            ],
+        },
+    ],
+}
+
+export class DropdownTreeSelectDemo extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value:
+            {
+                "checked": "true",
+                "hide": "false",
+                "label": "searchme2",
+                "value": "searchme2"
+            }
+
+        }
+    }
+    componentDidMount() {
+        // setTimeout(() =>{
+        //     alert('hello')
+        //     this.setState({value: a})
+        // },8000)
+    }
+    render() {
+        return (
+            <div>
+                <Form>
+                    <FormGroup row>
+                        <Label for="exampleEmail" sm={2} size="lg">Email</Label>
+
+                        <Input type="email" name="email" id="exampleEmail" placeholder="lg" bsSize="lg" />
+
+                    </FormGroup>
+                </Form>
+                <XDropdownMultiTreeSelect defaultList={a} value={this.state.value} onChange={(a) => { console.log(a); this.setState({ value: a }) }} />
+                <XDropdownTreeSelect defaultList={a} value={this.state.value} onChange={(a) => { console.log(a); this.setState({ value: a }) }} />
+                <XDropdownTreeSelectResult defaultList={a} value={this.state.value} onChange={(a) => { console.log(a); this.setState({ value: a }) }} />
+            </div>
+        )
+    }
+}
