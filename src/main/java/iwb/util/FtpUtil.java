cc13e31c17916136e85b9219f5c2862c8414501b
Ulugbek Irmatov <bekathegooner@gmail.com>
2019-12-19 17:41:24 +0300

    ftp util

---
+++ src/main/java/iwb/util/FtpUtil.java
@@ -0,0 +1,120 @@
+package iwb.util;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import iwb.cache.FrameworkSetting;
+import iwb.exception.IWBException;
+import sun.net.www.protocol.ftp.FtpURLConnection;
+
+public class FtpUtil {
+
+	public static String send(String targetURL) {
+		FtpURLConnection connection = null;
+		try {
+			URL url = new URL(targetURL);
+			connection = (FtpURLConnection) url.openConnection();
+
+			connection.setUseCaches(false);
+			connection.setDoInput(true);
+			connection.setDoOutput(true);
+
+			// Send request
+
+
+			// Get Response
+			boolean success = connection.getContentLength() != 0;
+			
+			if(success) {
+				InputStream is = connection.getInputStream();
+
+				
+				BufferedReader rd = new BufferedReader(new InputStreamReader(is,"UTF-8"));
+				String line;
+				StringBuilder response = new StringBuilder();
+				while ((line = rd.readLine()) != null) {
+					response.append(line);
+					response.append('\r');
+				}
+				rd.close();
+				return response.toString();
+			}
+			return null;
+
+		} catch (Exception e) {
+			if(FrameworkSetting.debug)e.printStackTrace();
+			 throw new IWBException(
+			          "framework",
+			          "HTTPUtil.send",
+			          0,
+			          null,
+			          targetURL ,
+			          e);
+			
+//			throw ne;
+		} finally {
+			if (connection != null) {
+				connection.close();
+			}
+		}
+	}
+	
+	public static byte[] send4bin(String targetURL) {
+		FtpURLConnection connection = null;
+		try {
+			URL url = new URL(targetURL);
+			connection = (FtpURLConnection) url.openConnection();
+			connection.setUseCaches(false);
+			connection.setDoInput(true);
+			connection.setDoOutput(true);
+
+			// Send request
+			
+
+			// Get Response
+			InputStream is = connection.getInputStream();
+			
+			
+			int maxLen = 32*1024*1024; //32mb
+			byte[] buffer = new byte[maxLen];
+		    int bytesRead, offset = 0;
+		    while ((bytesRead = is.read(buffer, offset, 1024)) != -1 && offset < maxLen-1024) {
+		        offset += bytesRead;
+		    }
+		    byte[] result = new byte[offset];
+			for(int qi=0;qi<offset;qi++)result[qi] = buffer[qi]; 
+		    return result;
+
+		} catch (Exception e) {
+			if(FrameworkSetting.debug)e.printStackTrace();
+			 throw new IWBException(
+			          "framework",
+			          "HTTPUtil.send",
+			          0,
+			          null,
+			          targetURL,
+			          e);
+			
+//			throw ne;
+		} finally {
+			if (connection != null) {
+				connection.close();
+			}
+		}
+	}
+	
+}
+
